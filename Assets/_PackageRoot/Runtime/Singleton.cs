ae4b962f643d608822e59bf91fafff613c79e6d1
Andrey <40003779+KelpieT@users.noreply.github.com>
2023-11-16 17:05:27 +0200

    Add Singleton

---
+++ Assets/_PackageRoot/Runtime/Singleton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Viter
+{
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T instance;
+
+        public static T Istance { get => instance; }
+
+        protected virtual void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+            }
+            else
+            {
+                if (instance != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+        
+    }
+}
